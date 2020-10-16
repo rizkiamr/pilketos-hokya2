a00b190afe6a422165ea0307cf5280c88ceea889
Rzky <48115312+rzii@users.noreply.github.com>
2020-10-16 12:37:01 +0700

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'https://www.smea-pilketos.ga';
+$config['base_url'] = 'https://smea-pilketos.ga';
 
 /*
 |--------------------------------------------------------------------------
